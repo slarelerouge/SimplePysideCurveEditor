2f64911b1455404f93b3a8e30abcb164448fdd17
Paul Arnould <arnouldpaulga@gmail.com>
2022-02-14 00:33:21 +0100

    update

--- main.py
+++ main.py
@@ -1,69 +1,43 @@
 # IMPORT
-import pathlib
 import numpy as np
-from OpenGL import GL
-from PySide2.QtWidgets import QOpenGLWidget, QApplication
-from PySide2.QtOpenGL import QGLWidget
-from PySide2.QtCore import *
-import ctypes
+from PySide2.QtWidgets import QApplication
 import math
+from small_renderer import *
 
 
 # FUNCTION
+def distance_sq(pos_a, pos_b):
+    x_dif = abs(pos_a[0] - pos_b[0])
+    y_dif = abs(pos_a[1] - pos_b[1])
+    return math.pow(x_dif, 2) + math.pow(y_dif, 2)
 
 
 # CLASS
-class OpenGLWidget(QOpenGLWidget):
+class CurveEditor(OpenGLWidget):
     def __init__(self, *args, **kwargs):
-        QOpenGLWidget.__init__(self, *args, **kwargs)
-
-        # Set update timer
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update)
+        super().__init__(*args, **kwargs)
 
         self.line_data = Line(color=(1.0, 0.3, 0.3))
         self.line_data.add_point((0.0, 0.0))
         self.line_data.add_point((0.5, 0.5))
+        self.line_data.add_point((1.0, 1.0))
 
         self.selected_point = None
 
-
     def initializeGL(self):
-        # Prepare openGL
-        background = Entity(draw_method=GL.GL_TRIANGLES)
-
-        # Format pos.xyz, color.xyz
-        v_data_background = np.array(
-            [1.0, 1.0, 0.5,  0.5, 0.5, 0.5,
-             -1.0, 1.0, 0.5,  0.5, 0.5, 0.5,
-             -1.0, -1.0, 0.5,  0.5, 0.5, 0.5,
-             1.0, 1.0, 0.5,  0.5, 0.5, 0.5,
-             -1.0, -1.0, 0.5,  0.5, 0.5, 0.5,
-             1.0, -1.0, 0.5,  0.5, 0.5, 0.5],
-            dtype=np.float32)
-        background.set_vertices_data(v_data_background)
-
+        super().initializeGL()
         v_data_line = self.line_data.get_vertex_array()
 
         line = Entity(draw_method=GL.GL_LINES)
         line.set_vertices_data(v_data_line)
 
-        self.entities = [line]
-
-        # Clear color
-        GL.glClearColor(0.2, 0.3, 0.3, 1.0)
-
-        # Set refresh rate
-        self.timer.start(33)
+        self.add_entity(line)
 
     def paintGL(self):
-        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
 
         v_data_line = self.line_data.get_vertex_array()
         self.entities[0].update_vertices_data(v_data_line)
-
-        for entity in self.entities:
-            entity.draw()
+        super().paintGL()
 
     def mousePressEvent(self, event):
         mouse_pos = event.pos().toTuple()
@@ -74,14 +48,21 @@ class OpenGLWidget(QOpenGLWidget):
     def mouseMoveEvent(self, event):
         mouse_pos = event.pos().toTuple()
         size = self.size().toTuple()
-        pos = (2 * mouse_pos[0] / size[0] - 1, -(2 * mouse_pos[1] / size[1] - 1))
+        pos = (max(min(2 * mouse_pos[0] / size[0] - 1, 1), -1), max(min(-(2 * mouse_pos[1] / size[1] - 1), 1), -1))
 
         if self.selected_point != None:
             self.line_data.points[self.selected_point].move(pos)
+            self.selected_point = self.line_data.reorder(self.selected_point)
 
     def mouseReleaseEvent(self, event):
         self.selected_point = None
 
+    def mouseDoubleClickEvent(self, event):
+        mouse_pos = event.pos().toTuple()
+        size = self.size().toTuple()
+        pos = (2 * mouse_pos[0] / size[0] - 1, -(2 * mouse_pos[1] / size[1] - 1))
+        self.line_data.add_point(pos)
+        self.line_data.reorder()
 
 class Line:
     def __init__(self, color=(1.0, 1.0, 1.0)):
@@ -91,10 +72,31 @@ class Line:
     def add_point(self, pos=(0, 0)):
         self.points.append(Point(pos=pos, color=self.color))
 
+    def reorder(self, current_point=0):
+        for i in range(len(self.points)):
+            if i != len(self.points)-1:
+                if self.points[i+1].pos[0] < self.points[i].pos[0]:
+                    item = self.points.pop(i + 1)
+                    self.points.insert(i, item)
+                    if current_point == i:
+                        current_point += 1
+                    elif current_point == i+1:
+                        current_point -= 1
+        return current_point
+
     def get_vertex_array(self):
         out = []
+        i=0
         for point in self.points:
+            if len(out) > 10:
+                out += list(self.points[i-1].pos) + [0.5] + list(self.points[i-1].color)
+                pass
             out += list(point.pos) + [0.5] + list(point.color)
+            i += 1
+        out += list(self.points[-1].pos) + [0.5] + list(self.points[-1].color)
+        out += list((1, self.points[-1].pos[1])) + [0.5] + list(self.points[-1].color)
+        out = list(self.points[0].pos) + [0.5] + list(self.points[0].color) + out
+        out = list((-1, self.points[0].pos[1])) + [0.5] + list(self.points[0].color) + out
         return np.array(out, dtype=np.float32)
 
     def get_close_point(self, pos, margin=0.02):
@@ -114,10 +116,6 @@ class Line:
             i += 1
         return closest_id
 
-def distance_sq(pos_a, pos_b):
-    x_dif = abs(pos_a[0] - pos_b[0])
-    y_dif = abs(pos_a[1] - pos_b[1])
-    return math.pow(x_dif, 2) + math.pow(y_dif, 2)
 
 class Point:
     def __init__(self, pos=(0, 0), color=(1, 0.3, 0.3)):
@@ -128,82 +126,9 @@ class Point:
         self.pos = pos
 
 
-class ShaderProgram:
-    def __init__(self, vs="vert.glsl", fs="frag.glsl"):
-        vertex_shader_source_path = pathlib.Path("glsl/" + vs)
-        fragment_shader_source_path = pathlib.Path("glsl/" + fs)
-
-        with open(vertex_shader_source_path, 'r') as vertex_shader_source:
-            vertex_shader_code = vertex_shader_source.read()
-        with open(fragment_shader_source_path, 'r') as fragment_shader_source:
-            fragment_shader_code = fragment_shader_source.read()
-
-        vertex_shader = GL.glCreateShader(GL.GL_VERTEX_SHADER)
-        GL.glShaderSource(vertex_shader, vertex_shader_code)
-        GL.glCompileShader(vertex_shader)
-
-        fragment_shader = GL.glCreateShader(GL.GL_FRAGMENT_SHADER)
-        GL.glShaderSource(fragment_shader, fragment_shader_code)
-        GL.glCompileShader(fragment_shader)
-
-        self.shader_program = GL.glCreateProgram()
-
-        GL.glAttachShader(self.shader_program, vertex_shader)
-        GL.glAttachShader(self.shader_program, fragment_shader)
-        GL.glLinkProgram(self.shader_program)
-
-    def use(self):
-        GL.glUseProgram(self.shader_program)
-
-
-class Entity:
-    def __init__(self, draw_method=GL.GL_TRIANGLES):
-        self.shader_program = ShaderProgram()
-
-        self.vbo = GL.glGenBuffers(1)
-        self.vao = GL.glGenVertexArrays(1)
-
-        self.draw_method = draw_method
-        self.length = int(0)
-
-    def update_vertices_data(self, v_data):
-        # Format pos.xyz, color.xyz
-        self.vertices = v_data
-        self.length = int(len(v_data) / 6)
-
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
-        GL.glBufferData(GL.GL_ARRAY_BUFFER, v_data.nbytes, v_data, GL.GL_STATIC_DRAW)
-
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
-
-    def set_vertices_data(self, v_data):
-        # Format pos.xyz, color.xyz
-        self.vertices = v_data
-        self.length = int(len(v_data)/6)
-
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
-        GL.glBufferData(GL.GL_ARRAY_BUFFER, v_data.nbytes, v_data, GL.GL_STATIC_DRAW)
-
-        #
-        GL.glBindVertexArray(self.vao)
-        # Position
-        GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, GL.GL_FALSE, 4*3*2, ctypes.cast(0, ctypes.c_void_p))
-        GL.glEnableVertexAttribArray(0)
-        # Color
-        GL.glVertexAttribPointer(1, 3, GL.GL_FLOAT, GL.GL_FALSE, 4*3*2, ctypes.cast(4*3, ctypes.c_void_p))
-        GL.glEnableVertexAttribArray(1)
-
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
-
-    def draw(self):
-        self.shader_program.use()
-        GL.glBindVertexArray(self.vao)
-        GL.glDrawArrays(self.draw_method, 0, self.length)
-
-
 app = QApplication([])
 
-widget = OpenGLWidget()
+widget = CurveEditor()
 widget.show()
 
 app.exec_()
---
+++ small_renderer.py
@@ -0,0 +1,116 @@
+# IMPORT
+import pathlib
+from OpenGL import GL
+from PySide2.QtWidgets import QOpenGLWidget
+from PySide2.QtCore import *
+import ctypes
+
+
+# FUNCTION
+
+
+# CLASS
+class OpenGLWidget(QOpenGLWidget):
+    def __init__(self, *args, **kwargs):
+        QOpenGLWidget.__init__(self, *args, **kwargs)
+
+        # Set update timer
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+
+        self.entities = []
+
+    def add_entity(self, entity):
+        self.entities.append(entity)
+
+    def initializeGL(self):
+        # Clear color
+        GL.glClearColor(0.2, 0.3, 0.3, 1.0)
+        # Set refresh rate
+        self.timer.start(33)
+
+    def paintGL(self):
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+
+        for entity in self.entities:
+            entity.draw()
+
+
+class ShaderProgram:
+    def __init__(self, vs="vert.glsl", fs="frag.glsl"):
+        vertex_shader_source_path = pathlib.Path("glsl/" + vs)
+        fragment_shader_source_path = pathlib.Path("glsl/" + fs)
+
+        with open(vertex_shader_source_path, 'r') as vertex_shader_source:
+            vertex_shader_code = vertex_shader_source.read()
+        with open(fragment_shader_source_path, 'r') as fragment_shader_source:
+            fragment_shader_code = fragment_shader_source.read()
+
+        vertex_shader = GL.glCreateShader(GL.GL_VERTEX_SHADER)
+        GL.glShaderSource(vertex_shader, vertex_shader_code)
+        GL.glCompileShader(vertex_shader)
+
+        fragment_shader = GL.glCreateShader(GL.GL_FRAGMENT_SHADER)
+        GL.glShaderSource(fragment_shader, fragment_shader_code)
+        GL.glCompileShader(fragment_shader)
+
+        self.shader_program = GL.glCreateProgram()
+
+        GL.glAttachShader(self.shader_program, vertex_shader)
+        GL.glAttachShader(self.shader_program, fragment_shader)
+        GL.glLinkProgram(self.shader_program)
+
+    def use(self):
+        GL.glUseProgram(self.shader_program)
+
+
+class Entity:
+    def __init__(self, draw_method=GL.GL_TRIANGLES):
+        self.shader_program = ShaderProgram()
+
+        self.vbo = GL.glGenBuffers(1)
+        self.vao = GL.glGenVertexArrays(1)
+
+        self.draw_method = draw_method
+        self.length = int(0)
+
+    def update_vertices_data(self, v_data):
+        # Format pos.xyz, color.xyz
+        self.vertices = v_data
+        self.length = int(len(v_data) / 6)
+
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
+        GL.glBufferData(GL.GL_ARRAY_BUFFER, v_data.nbytes, v_data, GL.GL_STATIC_DRAW)
+
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+
+    def set_vertices_data(self, v_data, buffer_format=[3, 3]):
+        # Format pos.xyz, color.xyz
+        self.vertices = v_data
+        self.length = int(len(v_data)/6)
+        self.byte_size = 4
+
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
+        GL.glBufferData(GL.GL_ARRAY_BUFFER, v_data.nbytes, v_data, GL.GL_STATIC_DRAW)
+
+        #
+        GL.glBindVertexArray(self.vao)
+
+        stride = 0
+        for elem_number in buffer_format:
+            stride += self.byte_size*elem_number
+
+        i=0
+        offset = 0
+        for elem_number in buffer_format:
+            GL.glVertexAttribPointer(i, elem_number, GL.GL_FLOAT, GL.GL_FALSE, stride, ctypes.cast(offset, ctypes.c_void_p))
+            GL.glEnableVertexAttribArray(i)
+            offset = self.byte_size*elem_number
+            i+=1
+
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+
+    def draw(self):
+        self.shader_program.use()
+        GL.glBindVertexArray(self.vao)
+        GL.glDrawArrays(self.draw_method, 0, self.length)
