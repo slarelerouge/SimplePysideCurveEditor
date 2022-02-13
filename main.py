888491ee5003dd5cdb6e5547ec3d43ca557ed43f
Paul Arnould <arnouldpaulga@gmail.com>
2022-02-13 14:10:07 +0100

    udpate

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def create_vertices_data():
     background_vertices = np.array([1.0, 1.0, 0.5, -1.0, 1.0, 0.5, -1.0, -1.0, 0.5, 1.0, 1.0, 0.5, -1.0, -1.0, 0.5, 1.0, -1.0, 0.5], dtype=np.float32)
     line_vertices = np.array([0.0, 0.0, 0.5, 1.0, 1.0, 0.5], dtype=np.float32)
     vertices = np.concatenate((background_vertices, line_vertices))
-    return vertices
+    return background_vertices
 
 
 # CLASS
@@ -25,12 +25,25 @@ class OpenGLWidget(QOpenGLWidget):
         self.timer.timeout.connect(self.update)
 
     def initializeGL(self):
-        self.OGL = OGL()
+        # Prepare openGL
+        background = Entity(draw_method=GL.GL_TRIANGLES)
+        v_data_background = np.array(
+            [1.0, 1.0, 0.5, -1.0, 1.0, 0.5, -1.0, -1.0, 0.5, 1.0, 1.0, 0.5, -1.0, -1.0, 0.5, 1.0, -1.0, 0.5],
+            dtype=np.float32)
+        background.set_vertices_data(v_data_background)
+
+        line = Entity(draw_method=GL.GL_LINES)
+        v_data_line = np.array([0.0, 0.0, 0.5, 1.0, 1.0, 0.5], dtype=np.float32)
+        line.set_vertices_data(v_data_line)
+
+        self.entities = [background, line]
+
         # Set refresh rate
         self.timer.start(33)
 
     def paintGL(self):
-        self.OGL.draw()
+        for entity in self.entities:
+            entity.draw()
 
 
 class ShaderProgram:
@@ -62,95 +75,35 @@ class ShaderProgram:
 
 
 class Entity:
-    def __init__(self):
+    def __init__(self, draw_method=GL.GL_TRIANGLES):
         self.vertices = []
+
+        self.shader_program = ShaderProgram()
+
         self.vbo = GL.glGenBuffers(1)
         self.vao = GL.glGenVertexArrays(1)
 
+        self.draw_method = draw_method
+        self.length = int(0)
+
     def set_vertices_data(self, v_data):
         self.vertices = v_data
+        self.length = int(len(v_data)/3)
+
         GL.glBindVertexArray(self.vao)
         GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
         GL.glBufferData(GL.GL_ARRAY_BUFFER, v_data.nbytes, v_data, GL.GL_STATIC_DRAW)
 
-
         GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, GL.GL_FALSE, 0, None)
         GL.glEnableVertexAttribArray(0)
-
-
-
 
         GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
         GL.glBindVertexArray(0)
 
-
-class OGL:
-    def __init__(self):
-        self.shader_program = ShaderProgram()
-        self.shader_program.use()
-
-        entity = Entity()
-        v_data = create_vertices_data()
-        print(v_data)
-        entity.set_vertices_data(v_data)
-        self.entities = [entity]
-
-
-        """self._init_shader_program()
-        self._init_array_buffer()
-
-        self._va_index = {"vertex": 0, "color": 1}
-        #self._init_background()"""
-
-    def _init_shader_program(self):
-        vertex_shader_source_path = pathlib.Path("glsl/vert.glsl")
-        fragment_shader_source_path = pathlib.Path("glsl/frag.glsl")
-
-        with open(vertex_shader_source_path, 'r') as vertex_shader_source:
-            vertex_shader_code = vertex_shader_source.read()
-        with open(fragment_shader_source_path, 'r') as fragment_shader_source:
-            fragment_shader_code = fragment_shader_source.read()
-
-        vertex_shader = GL.glCreateShader(GL.GL_VERTEX_SHADER)
-        GL.glShaderSource(vertex_shader, vertex_shader_code)
-        GL.glCompileShader(vertex_shader)
-
-        fragment_shader = GL.glCreateShader(GL.GL_FRAGMENT_SHADER)
-        GL.glShaderSource(fragment_shader, fragment_shader_code)
-        GL.glCompileShader(fragment_shader)
-
-        self.shader_program = GL.glCreateProgram()
-
-        GL.glAttachShader(self.shader_program, vertex_shader)
-        GL.glAttachShader(self.shader_program, fragment_shader)
-        GL.glLinkProgram(self.shader_program)
-
-        GL.glUseProgram(self.shader_program)
-
-    def _init_array_buffer(self):
-        # Create generic buffer
-        self.vertex_buffer = GL.glGenBuffers(1)
-        self.color_buffer = GL.glGenBuffers(1)
-
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vertex_buffer)
-        vertices = create_vertices_data()
-        GL.glBufferData(GL.GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL.GL_STATIC_DRAW)
-
-        GL.glEnableVertexAttribArray(0)
-        self.vao = GL.glGenVertexArrays(1)
-        GL.glBindVertexArray(self.vao)
-        GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, GL.GL_FALSE, 0, None)
-
-    def _create_color_data(self):
-        background_color = np.array(
-            [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5], dtype=np.float32)
-        return background_color
-
     def draw(self):
-        #GL.glEnableVertexAttribArray(self._va_index["vertex"])
-        #GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
-        GL.glBindVertexArray(self.entities[0].vao)
-        GL.glDrawArrays(GL.GL_LINES, 6, 10)
+        self.shader_program.use()
+        GL.glBindVertexArray(self.vao)
+        GL.glDrawArrays(self.draw_method, 0, self.length)
 
 
 app = QApplication([])
